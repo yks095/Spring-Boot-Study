a79ffdde774f6c7af565e1a83b898d71ffc27127
yks095 <rltjr219@naver.com>
2019-01-11 16:59:36 +0900

    Day 3 commit

---
+++ Day 3/community/src/main/java/com/community/service/BookService.java
@@ -0,0 +1,8 @@
+package com.community.service;
+
+import com.community.domain.Book;
+import java.util.List;
+
+public interface BookService {
+    List<Book> getBookList();
+}
---
+++ Day 3/community/src/test/java/com/community/BookControllerTest.java
@@ -0,0 +1,48 @@
+package com.community;
+
+import com.community.controller.BookController;
+import com.community.domain.Book;
+import com.community.service.BookService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import static org.hamcrest.Matchers.contains;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(BookController.class)
+public class BookControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private BookService bookService;
+
+    @Test
+    public void Book_MVC_테스트() throws Exception {
+        Book book = new Book("Spring Boot Book", LocalDateTime.now());
+        given(bookService.getBookList()).willReturn(Collections.singletonList(book));
+
+        mvc.perform(get("/books"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("book"))
+                .andExpect(model().attributeExists("bookList"))
+                .andExpect(model().attribute("bookList", contains(book)));
+    }
+
+
+}
