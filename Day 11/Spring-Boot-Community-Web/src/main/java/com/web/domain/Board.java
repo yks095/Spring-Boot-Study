2db9473a58d0ecda1dc251e19bc3e8e0ebccc326
yks095 <rltjr219@naver.com>
2019-02-06 20:51:53 +0900

    Day 11 update

---
+++ Day 11/Spring-Boot-Community-Web/src/main/java/com/web/domain/Board.java
@@ -0,0 +1,110 @@
+//package com.web.domain;
+//
+//import com.web.domain.enums.BoardType;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.*;
+//import java.io.Serializable;
+//import java.time.LocalDateTime;
+//
+//@Getter
+//@NoArgsConstructor
+//@Entity
+//@Table
+//public class Board implements Serializable {
+//    @Id
+//    @Column
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long idx;
+//
+//    @Column
+//    private String title;
+//
+//    @Column
+//    private String subTitle;
+//
+//    @Column
+//    private String content;
+//
+//    @Column
+//    @Enumerated(EnumType.STRING)
+//    private BoardType boardType;
+//
+//    @Column
+//    private LocalDateTime createdDate;
+//
+//    @Column
+//    private LocalDateTime updatedDate;
+//
+//    @OneToOne(fetch = FetchType.LAZY)
+//    private User user;
+//
+//    @Builder
+//    public Board(String title, String subTitle, String content, BoardType boardType, LocalDateTime createdDate, LocalDateTime updatedDate, User user) {
+//        this.title = title;
+//        this.subTitle = subTitle;
+//        this.content = content;
+//        this.boardType = boardType;
+//        this.createdDate = createdDate;
+//        this.updatedDate = updatedDate;
+//        this.user = user;
+//    }
+//}
+package com.web.domain;
+
+import com.web.domain.enums.BoardType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.apache.tomcat.jni.Local;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table
+public class Board implements Serializable {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+    //DB의 필드
+    @Column
+    private String title;
+
+    @Column
+    private String subTitle;
+
+    @Column
+    private String content;
+
+    @Column
+    @Enumerated(EnumType.STRING)    // String 타입으로
+    private BoardType boardType;
+
+    @Column
+    private LocalDateTime createdDate;
+
+    @Column
+    private LocalDateTime updatedDate;
+
+    @OneToOne(fetch = FetchType.LAZY)   // fetch - lazy
+    private User user;
+
+    @Builder
+    public Board(String title, String subTitle, String content, BoardType boardType, LocalDateTime createdDate, LocalDateTime updatedDate, User user) {
+        this.title = title;
+        this.subTitle = subTitle;
+        this.content = content;
+        this.boardType = boardType;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+        this.user = user;
+    }
+}
